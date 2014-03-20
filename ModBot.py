5ff3f78a70699460803c6ca867a32f2f0bec160e
C5E3B7BD (Will Apodaca) <C5E3B7BD@gmail.com>
2014-03-20 12:14:48 -0400

    Cleaned up some code (cleaned up file horrificness on the last commit) Fixed some of the login fallback logic. Added in the automated request thread feature. Remedied bulletpoints 3 and 4 on issue #2 (https://github.com/C5E3B7BD/Custom_ModBot/issues/2)
    Signed-off-by: C5E3B7BD (Will Apodaca) <C5E3B7BD@gmail.com>

--- ModBot.py
+++ ModBot.py
@@ -30,6 +30,8 @@ except:
             raise ValueError()
 try:
     password = config.get('AUTH', 'PASSWORD')
+    if password == '':
+        raise ValueError()
 except:
     try:
         import sys
@@ -78,18 +80,44 @@ starletNames = []
 flaggedFor=""
 miscVar = 0
 todayDate = None
+alreadyMadeAThreadThisMonth = False
+otherDate = datetime.date.today()
 #-----------------------------------------#
 
 #Main Loop
 #This one never exits
 #-----------------------------------------#
-while True:
+while True:              
     subreddit = r.get_subreddit(subR)
-    numlimit = 5
+    numlimit = 15
     smsn=subreddit.get_new(limit=numlimit)
     finished = False #Finished is set to true if/when we run into a submission that's contained in "already_done[]"
+
+    #Request thread logic
+    if datetime.date.today().day == 1:
+        if not alreadyMadeAThreadThisMonth:
+            uSubmissions = r.get_redditor(username).get_submitted()
+            threadString = str(datetime.datetime.now().strftime("%B")) + " " + str(datetime.date.today().year) + " Request Thread."
+            threadText = "Remember, please post your celebrity requests here as a comment, **ONE NAME ONLY PER REQUEST**, so that each name can be voted on separately.\n\n**HEADSHOTS (HS) MUST BE PHOTOS TAKEN OF CELEBRITIES AFTER HAVING ALREADY TURNED 18**. \n\nIt is not okay to put a 15-year old's head on an 18-year old's body.\n\nNot here, at least.\n\nHere, that will get you immediately and permanently banned from this subreddit.\n\nThank you for your cooperation."
+            for sM in uSubmissions:
+                if sM.title == threadString:
+                    alreadyMadeAThreadThisMonth = True
+                else:
+                    requestSubmission = subreddit.submit(threadString, threadText)
+                    if not requestSubmission.stickied:
+                        if subreddit.reddit_session.user.is_mod:
+                            requestSubmission.sticky
+                    uS = r.get_redditor(username).get_submitted()
+                    '''submissionArray = []
+                    for userSubmission in uS:
+                        submissionArray.append(userSubmission)
+                    requestSubmission = submissionArray[0]
+                    if not requestSubmission.stickied:
+                        if subreddit.reddit_session.user.is_mod:
+                            requestSubmission.sticky'''
     while not finished:
         todayDate = datetime.date.today()
+        
         min_birthYear = str(todayDate.year-18)+"-"
         if todayDate.month < 10:
             min_birthYear += "0"+str(todayDate.month)+"-"+str(todayDate.day)
@@ -99,7 +127,9 @@ while True:
         additions = '&gender=female'
         
         IMDbLink = "http://www.imdb.com/search/name?birth_date=" + str(min_birthYear) + ',' + str(max_birthYear) + additions
-        starletNames = q.Age(min_birthYear,max_birthYear,additions)
+        if todayDate != otherDate:
+            starletNames = q.Age(min_birthYear,max_birthYear,additions)
+            otherDate = datetime.date.today()
         for submission in smsn:            
             if (submission.id in already_done):
                 finished = True
@@ -135,9 +165,10 @@ while True:
                         print("|"+str(len(submission.comments))+" comment "+spacerThing+'|')                    
 
                 testCaseThing = (submission.id not in already_done) and (flaggedFor != '')
+                #Check if the user has already commented in this thread, if so don't comment
+                #-----------------------------------------#
                 for commentitem in submission.comments:
                     try:
-                        #print('|'+commentitem)
                         boolThing = (username==commentitem.author.name)
                         if (boolThing):
                             print("|One of my comments                               |")
@@ -145,11 +176,15 @@ while True:
                             testCaseThing = False
                         else:
                             miscVar+=1
-                            #print("|Not one of my comments")
                     except AttributeError:
                         miscVar+=1
-                        #print("|Someone Deleted this comment, I don't know if it was me")
-                
+                        #Someone Deleted this comment, I don't know if it was me
+                #-----------------------------------------#
+                #        
+                #Test if the user has already done this submission, and that it's actually flagged for something
+                #Check if the user is a moderator in that subreddit, if so distinguish the comment
+                #Report the comment
+                #-----------------------------------------#
                 if testCaseThing == True:
                     if subreddit.reddit_session.user.is_mod:
                         submission.distinguish()
@@ -162,16 +197,16 @@ while True:
                     already_done.append(submission.id)
                     print(bottomSep)
                 already_done.append(submission.id)
-                time.sleep(2)
+                time.sleep(1)
+                #-----------------------------------------#
             already_done.append(submission.id)
-            time.sleep(5)
+            time.sleep(2)
         if (submission.id in already_done):
             finished = True
     try:
         already_done.append(submission.id)
     except:
-        #Something broke here
-        null=[]
+        shouldnt_Ever_Happen = []
     time.sleep(30)
                 
                 
