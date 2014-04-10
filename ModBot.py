1a7c68bcaed331158eb9d43d0a57433b4668eb23
C5E3B7BD (Will Apodaca) <C5E3B7BD@gmail.com>
2014-04-10 15:29:59 -0400

    Added remote shutdown and startup
    Fixed a date parsing issue.
    
    Signed-off-by: C5E3B7BD (Will Apodaca) <C5E3B7BD@gmail.com>

--- ModBot.py
+++ ModBot.py
@@ -82,6 +82,7 @@ miscVar = 0
 todayDate = None
 alreadyMadeAThreadThisMonth = False
 otherDate = datetime.date.today()
+donething = False
 #-----------------------------------------#
 
 #Main Loop
@@ -92,38 +93,50 @@ while True:
     numlimit = 15
     smsn=subreddit.get_new(limit=numlimit)
     finished = False #Finished is set to true if/when we run into a submission that's contained in "already_done[]"
-
-    #Request thread logic
-    if datetime.date.today().day == 1:
-        if not alreadyMadeAThreadThisMonth:
-            uSubmissions = r.get_redditor(username).get_submitted()
-            threadString = str(datetime.datetime.now().strftime("%B")) + " " + str(datetime.date.today().year) + " Request Thread."
-            threadText = "Remember, please post your celebrity requests here as a comment, **ONE NAME ONLY PER REQUEST**, so that each name can be voted on separately.\n\n**HEADSHOTS (HS) MUST BE PHOTOS TAKEN OF CELEBRITIES AFTER HAVING ALREADY TURNED 18**. \n\nIt is not okay to put a 15-year old's head on an 18-year old's body.\n\nNot here, at least.\n\nHere, that will get you immediately and permanently banned from this subreddit.\n\nThank you for your cooperation."
-            for sM in uSubmissions:
-                if sM.title == threadString:
-                    alreadyMadeAThreadThisMonth = True
-                else:
-                    requestSubmission = subreddit.submit(threadString, threadText)
-                    if not requestSubmission.stickied:
-                        if subreddit.reddit_session.user.is_mod:
-                            requestSubmission.sticky
-                    uS = r.get_redditor(username).get_submitted()
-                    '''submissionArray = []
-                    for userSubmission in uS:
-                        submissionArray.append(userSubmission)
-                    requestSubmission = submissionArray[0]
-                    if not requestSubmission.stickied:
-                        if subreddit.reddit_session.user.is_mod:
-                            requestSubmission.sticky'''
-    while not finished:
+##    #Request thread logic
+##    if datetime.date.today().day == 1:
+##        if not alreadyMadeAThreadThisMonth:
+##            uSubmissions = r.get_redditor(username).get_submitted()
+##            threadString = str(datetime.datetime.now().strftime("%B")) + " " + str(datetime.date.today().year) + " Request Thread."
+##            threadText = "Remember, please post your celebrity requests here as a comment, **ONE NAME ONLY PER REQUEST**, so that each name can be voted on separately.\n\n**HEADSHOTS (HS) MUST BE PHOTOS TAKEN OF CELEBRITIES AFTER HAVING ALREADY TURNED 18**. \n\nIt is not okay to put a 15-year old's head on an 18-year old's body.\n\nNot here, at least.\n\nHere, that will get you immediately and permanently banned from this subreddit.\n\nThank you for your cooperation."
+##            for sM in uSubmissions:
+##                if sM.title == threadString:
+##                    alreadyMadeAThreadThisMonth = True
+##                else:
+##                    requestSubmission = subreddit.submit(threadString, threadText)
+##                    if not requestSubmission.stickied:
+##                        if subreddit.reddit_session.user.is_mod:
+##                            requestSubmission.sticky
+##                    uS = r.get_redditor(username).get_submitted()
+##                    '''submissionArray = []
+##                    for userSubmission in uS:
+##                        submissionArray.append(userSubmission)
+##                    requestSubmission = submissionArray[0]
+##                    if not requestSubmission.stickied:
+##                        if subreddit.reddit_session.user.is_mod:
+##                            requestSubmission.sticky'''
+    inbox = r.get_unread(False, False)
+    for message in inbox:
+            if (inbox == []):
+                finished = True
+                break
+            else:
+                if '@start' in message.body:
+                    donething = False
+    while not finished and not donething:
+        inbox = r.get_unread(False, False)
+        for message in inbox:
+            if (inbox == []):
+                finished = True
+                break
+            else:
+                if '@shutdown' in message.body:
+                    donething = True
         todayDate = datetime.date.today()
-        
-        min_birthYear = str(todayDate.year-18)+"-"
-        if todayDate.month < 10:
-            min_birthYear += "0"+str(todayDate.month)+"-"+str(todayDate.day)
-        else:
-            min_birthYear += str(todayDate.month)+"-"+str(todayDate.day)
         max_birthYear = str(todayDate)
+        min_birthYear = str(datetime.date(todayDate.year-18,
+                                          todayDate.month,
+                                          todayDate.day))
         additions = '&gender=female'
         
         IMDbLink = "http://www.imdb.com/search/name?birth_date=" + str(min_birthYear) + ',' + str(max_birthYear) + additions
